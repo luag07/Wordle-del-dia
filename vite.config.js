209525f336e8430703cf4726f62c2525f80e7c2e
luag07 <144801051+luag07@users.noreply.github.com>
2025-06-29 19:09:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/wordle-del-dia/', // 👈 reemplazá por tu nombre de repositorio exacto si es distinto
+  base: '/Wordle-del-dia/', // 👈 reemplazá por tu nombre de repositorio exacto si es distinto
   plugins: [react()],
 })
